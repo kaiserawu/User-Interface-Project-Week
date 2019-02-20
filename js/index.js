2901dc71a0eb44fb409f571606d3bdc184c76a4e
kaiserawu <kaiser.a.wu@gmail.com>
2019-02-19 18:31:28 -0800

    Added open and close functionality to dropdown menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+
+// Header code below
+let navButton = document.querySelector('#nav-icon');
+let openImg = document.querySelector('.nav-open');
+let closeImg = document.querySelector('.nav-close');
+let navDropdown = document.querySelector('.nav-dropdown');
+navButton.addEventListener('click', () => {
+    navDropdown.classList.toggle('displayed');
+    openImg.classList.toggle('icon-hidden');
+    closeImg.classList.toggle('icon-hidden');
+})
