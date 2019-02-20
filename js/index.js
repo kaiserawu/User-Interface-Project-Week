c60142d5113cef0ca8b496940a5f8d9e0fc547ed
kaiserawu <kaiser.a.wu@gmail.com>
2019-02-19 19:33:59 -0800

    Added tab selection functionality for services page

--- js/index.js
+++ js/index.js
@@ -10,3 +10,37 @@ navButton.addEventListener('click', () => {
     openImg.classList.toggle('icon-hidden');
     closeImg.classList.toggle('icon-hidden');
 })
+
+// Services tab selection code below
+const tabButtonList = document.querySelectorAll('.tab-button');
+const tabContentList = document.querySelectorAll('.tab-content');
+
+class TabButton {
+    constructor(element) {
+        this.element = element;
+        this.data = this.element.dataset.tab;
+        this.contentElement = document.querySelector(`.tab-content[data-tab='${this.data}']`);
+        this.tabContent = new TabContent(this.contentElement);
+
+        this.element.addEventListener('click', () => { this.select(); })
+    }
+
+    select() {
+        Array.from(tabButtonList).forEach((button) => { button.classList.remove('tab-button-selected'); })
+        this.element.classList.add('tab-button-selected');
+        this.tabContent.select();
+    }
+}
+
+class TabContent {
+    constructor(element) {
+        this.element = element;
+    }
+
+    select() {
+        Array.from(tabContentList).forEach((content) => { content.classList.remove('tab-content-selected'); })
+        this.element.classList.add('tab-content-selected');
+    }
+}
+
+tabButtonList.forEach((button) => new TabButton(button));
